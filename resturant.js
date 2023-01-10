6f4a5a44c1bdce77ceae5f289a9d39fbf98628b6
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-10 17:43:24 +0200

    loading all products method

--- resturant.js
+++ resturant.js
@@ -5,4 +5,26 @@ class Restaurant {
         this.stockProducts = {};
         this.history = [];
     }
+
+    loadProducts(products) {
+        for (let element of products) {
+            let [product, quantity, price] = element.split(' ');
+            quantity = Number(quantity);
+            price = Number(price);
+
+            if (price <= this.budgetMoney) {
+                this.budgetMoney -= price;
+                this.history.push(`Successfully loaded ${quantity} ${product}`);
+
+                if (!this.stockProducts.hasOwnProperty(product)) {
+                    this.stockProducts[product] = 0;
+                }
+                this.stockProducts[product] += quantity;
+            } else {
+                this.history.push(`There was not enough money to load ${quantity} ${product}`);
+            }
+        }
+
+        return this.history.join('\n');
+    }
 }
