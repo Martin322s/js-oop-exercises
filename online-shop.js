1c70e8611e345578eab59bb1fb1d80d770646251
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-10 17:38:32 +0200

    loading store method

--- online-shop.js
+++ online-shop.js
@@ -4,4 +4,18 @@ class OnlineShop {
         this.products = [];
         this.sales = [];
     }
+
+    loadingStore(product, quantity, spaceRequired) {
+        if (this.wareHouseSpace < Number(spaceRequired)) {
+            throw new Error("Not enough space in the warehouse.");
+        } else {
+            const productElement = { product, quantity };
+
+            if (!this.products.find(x => x.product === product)) {
+                this.products.push(productElement);
+                this.wareHouseSpace = Number(this.wareHouseSpace) - Number(spaceRequired);
+                return `The ${product} has been successfully delivered in the warehouse.`;
+            }
+        }
+    }
 }
