389ed708c196763c21d345ad2955bd95d09dfd82
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-16 17:08:51 +0300

    new additions method

--- footballTeam.js
+++ footballTeam.js
@@ -4,4 +4,23 @@ class FootballTeam {
         this.country = country;
         this.invitedPlayers = [];
     }
+
+    newAdditions(footballPlayers) {
+        const newInvites = [];
+        for (let i = 0; i < footballPlayers.length; i++) {
+            const [name, age, playerValue] = footballPlayers[i].split('/');
+            const index = this.invitedPlayers.findIndex(p => p.name === name);
+            const player = { name, age: Number(age), playerValue: Number(playerValue) };
+            if (index !== -1) {
+                if (player.playerValue > this.invitedPlayers[index].playerValue) {
+                    this.invitedPlayers[index] = player;
+                }
+            } else {
+                this.invitedPlayers.push(player);
+                newInvites.push(name);
+            }
+        }
+        const message = `You successfully invite ${newInvites.join(', ')}.`;
+        return message;
+    }
 };
