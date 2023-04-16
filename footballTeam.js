f360e9ab5720285b7358bbb6b187ef5a708cbd15
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-16 17:09:43 +0300

    age limit method

--- footballTeam.js
+++ footballTeam.js
@@ -39,4 +39,23 @@ class FootballTeam {
         const message = `Congratulations! You sign a contract with ${name} for ${playerOffer} million dollars.`;
         return message;
     }
+
+    ageLimit(name, age) {
+        const index = this.invitedPlayers.findIndex(p => p.name === name);
+        if (index === -1) {
+            throw new Error(`${name} is not invited to the selection list!`);
+        }
+        const player = this.invitedPlayers[index];
+        const ageDifference = 18 + 5 - player.age;
+        if (player.age < age) {
+            if (ageDifference < 5) {
+                const message = `${name} will sign a contract for ${ageDifference} years with ${this.clubName} in ${this.country}!`;
+                return message;
+            }
+            const message = `${name} will sign a full 5 years contract for ${this.clubName} in ${this.country}!`;
+            return message;
+        }
+        const message = `${name} is above age limit!`;
+        return message;
+    }
 };
